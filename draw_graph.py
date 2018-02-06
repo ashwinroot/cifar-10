315e802ba1593155998a790ee06947aa60b270e0
Ashwin Sankar <ashwinoscar@gmail.com>
2018-02-06 12:26:10 -0700

    All variations

---
+++ draw_graph.py
@@ -0,0 +1,39 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+for x in range(0,2):
+    filename = 'objs'+str(x)+'.pickle'
+    with open(filename,'rb') as f:
+        fig = plt.figure()
+        eval_indices,output_indices,train_loss, testing_loss, test_accuracy, validation_accuracy = pickle.load(f)
+        # test_accuracy = np.subtract(1,test_accuracy)
+        # validation_accuracy = np.subtract(1,validation_accuracy)
+        plt.plot(output_indices, train_loss, 'k-', label='validation')
+        plt.title('Softmax Loss per Generation'+str(x))
+        plt.xlabel('Generation')
+        plt.ylabel('Softmax Loss')
+        fig.show()
+        fig.savefig("TrainLoss"+str(x))
+
+        fig1 = plt.figure()
+        plt.plot(output_indices, testing_loss,'k-', label='test')
+        plt.title('Test Loss ')
+        plt.xlabel('Generation')
+        plt.ylabel('Softmax Loss')
+        fig1.show()
+        fig1.savefig("TestLoss"+str(x))
+
+        # Plot accuracy over time
+        fig2 = plt.figure()
+        plt.plot(eval_indices, test_accuracy, 'r',label='test')
+        plt.plot(eval_indices,validation_accuracy,'b',label='train')
+        plt.legend()
+        plt.title('Accuracy comparison')
+        plt.xlabel('Generation')
+        plt.ylabel('Accuracy')
+        fig2.show()
+        fig2.savefig("Accuracy"+str(x))
+        print("Mean train accuracy : " , np.mean(validation_accuracy) , "Variance train accuracy : ", np.var(validation_accuracy))
+        print("Mean test accuracy : ", np.mean(test_accuracy), "Variance test accuracy : ",
+              np.var(test_accuracy))
